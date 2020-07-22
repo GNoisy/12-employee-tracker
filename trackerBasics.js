7fcb000f4767ef278862b569321fe43ed20b8c1b
Giovanni Noisy <gnoisy77@gmail.com>
2020-07-22 15:41:25 -0700

    changed inquirer prompt format

--- trackerBasics.js
+++ trackerBasics.js
@@ -1,6 +1,7 @@
 const mysql = require('mysql');
 const util = require("util");
 const inquirer = require("inquirer");
+const cTable = require('console.table');
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -16,11 +17,14 @@ var connection = mysql.createConnection({
     database: "employeeTracker_DB"
 });
 
-connection.connect(function(err) {
-    if (err) throw err;
-    console.log("connected as id " + connection.threadId);
-    init();
-});
+connection.connect();
+connection.queryPromise = util.promisify(connection.query);
+
+// connection.connect(function(err) {
+//     if (err) throw err;
+//     console.log("connected as id " + connection.threadId);
+//     init();
+// });
 
 function init() {
     inquirer
@@ -38,36 +42,25 @@ function init() {
         ]
     })
       .then(function(answer) {
-        switch (answer.action) {
-        case "Add Department":
+        if (answer.choice === "Add Department") {
             addDepartment();
-            break;
-
-        case "Add Role":
-            addRole();
-            break;
-
-        case "Add Employee":
-            addEmployee();
-            break;
-
-        case "View Department":
-            viewDepartment();
-            break;
-
-        case "View Role":
-            viewRole();
-            break;
-
-        case "View Employee":
-            viewEmployee();
-            break;
-
-        case "Update Employee Role":
-            updateEmployeeRole();
-            break;
         }
-    });
+        if (answer.choice === "Add Role") {
+            addRole();
+        }
+        if (answer.choice === "Add Employee") {
+            addEmployee();
+        }
+        if (answer.choice === "View Department") {
+            viewDepartment();
+        }
+        if (answer.choice === "View Role") {
+            viewRole();
+        }
+        if (answer.choice === "View Employee") {
+            viewEmployee();
+        }
+    })
 }
 init();
 
@@ -78,7 +71,75 @@ function addDepartment() {
       name: "name"
     })
       .then(function(answer) {
-        connection.query('INSERT INTO department (name) VALUES (?)', [answer.name]);
+        connection.queryPromise("INSERT INTO department (name) VALUES (?)", [answer.name]);
         init();
+      })
+}
+
+function addRole() {
+    // read the deparments table
+    connection.queryPromise("SELECT * FROM department")
+      .then(function(departments) {
+        return inquirer.prompt([
+          {
+            message: "Enter the title: ",
+            type: "input",
+            name: "title"
+          },
+          {
+            message: "Enter the salary",
+            type: "input",
+            name: "salary"
+          },
+          {
+            message: "Select the department: ",
+            type: "list",
+            name: "department_id",
+            choices: departments.map(function (department) {
+              return {
+                name: department.name,
+                value: department.id,
+              };
+            })
+          },
+        ])
+    })
+      .then(function(answer) {
+        console.log(answer);
+        connection.queryPromise("INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)", [answer.title, answer.salary, answer.department_id]);
+        init();
+      })
+}
+
+function addEmployee() {
+    connection.queryPromise("SELECT * FROM role")
+    .then(function(employees) {
+        return inquirer.prompt([
+        {
+            message: "Enter First Name",
+            type: "input",
+            name: first_name
+        },
+        {
+            message: "Enter Last Name",
+            type: "input",
+            name: last_name
+        },
+        {
+            message: "Enter Role ID",
+            type: "input",
+            name: role_id
+        },
+        {
+            message: "Enter Manager ID",
+            type: "input",
+            name: manager_id
+        },
+
+        ])
+    })
+      .then(function(answer) {
+          console.log(answer);
+          connection.queryPromise("INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)", [answer.first_name, answer.last_name, answer.role_id, answer.manager_id]);
       })
 }
