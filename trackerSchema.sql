afb0688a2dfb31a0c51f013cc446e3505a689c5d
Giovanni Noisy <gnoisy77@gmail.com>
2020-07-21 19:24:44 -0700

    updated file names and inital code for trackerSchema

---
+++ trackerSchema.sql
@@ -0,0 +1,26 @@
+DROP DATABASE IF EXISTS employeeTracker_DB;
+
+CREATE DATABASE employeeTracker_DB;
+
+USE employeeTracker_DB;
+
+CREATE TABLE department (
+  id INTEGER NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+title VARCHAR (30),
+salary DECIMAL(10,2),
+department_id INT
+);
+
+CREATE TABLE employee (
+id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+first_name VARCHAR(30),
+last_name VARCHAR(30),
+role_id INT,
+manager_id INT
+);
